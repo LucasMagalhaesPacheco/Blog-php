dd67279eceadde7e72489f453ba88e3abbb3ba8d
Lucas Magalhães Pacheco <lucasmagalhaespacheco@gmail.com>
2023-06-21 14:41:09 -0300

    Projeto-blog, header, footer, index

---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>Hora de codar</p>
+</footer>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,31 @@
+<?php
+include_once("helpers/url.php");
+include_once("data/posts.php");
+include_once("helpers/url.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog codar</title>
+    <!-- estilo do projeto -->
+    <link rel="stylesheet" href="<?php echo $BASE_URL ?>/css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+</head>
+<body>
+<header>
+<a href="<?php echo $BASE_URL ?>" id='logo'>
+<img src="<?php echo $BASE_URL?>/img/logo.svg" alt="Blog codar, logo"></a>
+<nav>
+    <ul id='navbar'>
+        <li><a href="<?php echo $BASE_URL ?>" class="nav-link">Home</a></li>
+        <li><a href="#" class="nav-link"> Categorias</a></li>
+        <li><a href="#" class="nav-link">Sobre</a></li>
+        <li><a href="<?php echo $BASE_URL ?>contact.php" class="nav-link">Contato</a></li>
+    </ul>
+</nav>
+</header>
