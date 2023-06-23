192dbc6fb877ae072dc12311ba8418da4ee7972c
Lucas Magalhães Pacheco <lucasmagalhaespacheco@gmail.com>
2023-06-23 15:47:18 -0300

    Finalizando o projeto Blog

--- post.php
+++ post.php
@@ -14,32 +14,30 @@
  }
 
 ?> 
- <main id='post-container'>
+ <main id="post-container">
 
-    <div class="content-container">
-        <h1 id="main-title"> <?php echo $currentPost['title'] ?></h1>
-        <p id="post-description"><?php echo $currentPost['description'] ?></p>
-        <div class="img-container">
-            <img src="<?php echo $BASE_URL ?>/img/<?php echo $currentPost['img'] ?>" alt="<?php echo $currentPost['title'] ?>">
-        </div>
-        <p class="post-content">
-            <?php echo $currentPost['content'] ?>
-        </p>
+ <div class="content-container">
+      <h1 id="main-title"><?= $currentPost['title'] ?></h1>
+      <p id="post-description"><?= $currentPost['description'] ?></p>
+      <div class="img-container">
+        <img src="<?= $BASE_URL ?>/img/<?= $currentPost['img'] ?>" alt="<?= $currentPost['title'] ?>">
+      </div>
+      <p class="post-content"><?php echo $currentPost['content'] ?></p>
     </div>
     <aside id="nav-container">
-    <h3 id="tags-title"> Tags</h3>
-    <ul id="tag-list">
-    <?php foreach($currentPost['tags'] as $tag): ?>
-        <li><a href="#"> <?php echo $tag ?></a></li>
-    <?php endforeach; ?>  
-    </ul> 
-    <h3 id="categories-title">Categorias</h3>
+      <h3 id="tags-title">Tags</h3>
+      <ul id="tag-list">
+        <?php foreach($currentPost['tags'] as $tag): ?>
+          <li><a href="#"><?= $tag ?></a></li>
+        <?php endforeach; ?>
+      </ul>
+      <h3 id="categories-title">Categorias</h3>
       <ul id="categories-list">
         <?php foreach($categories as $category): ?>
-          <li><a href="#"><?php echo $category ?></a></li>
+          <li><a href="#"><?= $category ?></a></li>
         <?php endforeach; ?>
-    </ul>
- </aside>
+      </ul>
+    </aside>
  </main>  
 <?php
  include_once("templates/footer.php");
